045c974a9bdd02d37ef8a6bbdba2783e093cb2bb
Herminio PC-Santa Clara <herminiopaucar@gmail.com>
2019-12-28 00:37:17 -0500

    Upload First Version

---
+++ OpenGL-tutorial-org/SecondProject/src/SecondProject.cpp
@@ -0,0 +1,134 @@
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+
+// Include GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+GLFWwindow* window;
+
+// Include GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+using namespace glm;
+
+#include "shader.hpp"
+
+GLuint programID;
+GLuint MatrixID;
+GLuint vertexbuffer;
+GLuint VertexArrayID;
+
+glm::mat4 Model;
+glm::mat4 MVP;
+
+void setupVertices(void)
+{
+    // Dark blue background
+    glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
+
+    // Create and compile our GLSL program from the shaders
+    programID = LoadShaders( "src/SimpleTransform.vertexshader", "src/SingleColor.fragmentshader" );
+
+    // Get a handle for our "MVP" uniform
+    MatrixID = glGetUniformLocation(programID, "MVP");
+
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+    // Or, for an ortho camera :
+    //glm::mat4 Projection = glm::ortho(-10.0f,10.0f,-10.0f,10.0f,0.0f,100.0f); // In world coordinates
+
+    // Camera matrix
+    glm::mat4 View = glm::lookAt(
+                         glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+                         glm::vec3(0,0,0), // and looks at the origin
+                         glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+                     );
+    // Model matrix : an identity matrix (model will be at the origin)
+    Model = glm::mat4(1.0f);
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    MVP = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+    static const GLfloat g_vertex_buffer_data[] =
+    {
+        -1.0f, -1.0f, 0.0f,
+        1.0f, -1.0f, 0.0f,
+        0.0f,  1.0f, 0.0f,
+    };
+
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+}
+
+void init(GLFWwindow* window)
+{
+    // Ensure we can capture the escape key being pressed below
+    glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+    setupVertices();
+}
+
+void display(GLFWwindow* window, double currentTime)
+{
+    // Clear the screen
+    glClear( GL_COLOR_BUFFER_BIT );
+
+    // Use our shader
+    glUseProgram(programID);
+
+    // Send our transformation to the currently bound shader,
+    // in the "MVP" uniform
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+    // 1rst attribute buffer : vertices
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer(
+        0,                  // attribute. No particular reason for 0, but must match the layout in the shader.
+        3,                  // size
+        GL_FLOAT,           // type
+        GL_FALSE,           // normalized?
+        0,                  // stride
+        (void*)0            // array buffer offset
+    );
+
+    // Draw the triangle !
+    glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
+
+    glDisableVertexAttribArray(0);
+}
+
+int main(void)   //4 main() is unchanged from before
+{
+    if (!glfwInit())
+    {
+        exit(EXIT_FAILURE);
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    GLFWwindow* window = glfwCreateWindow(600, 600, "TEST CLASS MODE", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (glewInit() != GLEW_OK)
+    {
+        exit(EXIT_FAILURE);
+    }
+    glfwSwapInterval(1);
+    init(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
