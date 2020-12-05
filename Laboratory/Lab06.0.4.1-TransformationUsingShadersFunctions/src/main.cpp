fb9a35cc409887b532ca37a85b3a8196b9625d51
Herminio PC-Santa Clara <herminiopaucar@gmail.com>
2020-12-04 21:12:25 -0500

    Transformation using shaders

---
+++ Laboratory/Lab06.0.4.1-TransformationUsingShadersFunctions/src/main.cpp
@@ -0,0 +1,129 @@
+//============================================================================
+// Name        : Display Animation using Shaders
+// Professor   : Herminio Paucar
+// Version     :
+// Description : Display my first Animation
+//============================================================================
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+// Include GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+#include <string>
+#include <fstream>
+
+#include "Utils.h"
+
+GLuint renderingProgram;
+GLfloat* m_Vertices;
+GLuint n_Vertices;
+GLuint m_VBO;
+GLuint m_VAO;
+
+// location of triangle on x axis
+float x = 0.0f;
+// offset for moving the triangle
+float inc = 0.01f;
+
+using namespace std;
+
+void init () {
+	renderingProgram = Utils::createShaderProgram("src/vertShader.glsl", "src/fragShader.glsl");
+
+	// Cria um ID na GPU para um array de  buffers
+	glGenVertexArrays(1, &m_VAO);
+	glBindVertexArray(m_VAO);
+
+    // The first 3 points are to Vertex position of Triangle
+	m_Vertices = new GLfloat[9] {
+		-1.0f, -1.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f,
+		 0.0f,  1.0f, 0.0f
+	};
+	n_Vertices = 9;
+	// Cria um ID na GPU para nosso buffer
+	glGenBuffers(1, &m_VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
+
+	// Reserva memoria na GPU para um TARGET receber dados
+	// Copia esses dados pra essa área de memoria
+	glBufferData(
+			GL_ARRAY_BUFFER,	// TARGET associado ao nosso buffer
+			n_Vertices * sizeof(GLfloat),	// tamanho do buffer
+			m_Vertices,			// Dados a serem copiados pra GPU
+			GL_STATIC_DRAW		// Política de acesso aos dados, para otimização
+		);
+
+	glVertexAttribPointer(
+			0,			// Lembra do (layout = 0 ) no vertex shader ? Esse valor indica qual atributo estamos indicando
+			3,			// cada vertice é composto de 3 valores
+			GL_FLOAT,	// cada valor do vértice é do tipo GLfloat
+			GL_FALSE,	// Quer normalizar os dados e converter tudo pra NDC ? ( no nosso caso, já esta tudo correto, então deixamos como FALSE)
+			0 * sizeof(GLfloat),// De quantos em quantos bytes, este atributo é encontrado no buffer ? No nosso caso 3 floats pros vertices + 3 floats pra cor = 6 floats
+			(GLvoid*) 0	// Onde está o primeiro valor deste atributo no buffer. Nesse caso, está no início do buffer
+		);
+	glEnableVertexAttribArray(0);	// Habilita este atributo
+}
+
+void display(double currentTime) {
+    glUseProgram(renderingProgram);
+    
+    // clear the background to black, each time
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // move the triangle along x axis
+    x += inc;
+    // switch to moving the triangle to the left
+    if (x > 1.0f) inc = -0.01f;
+    // switch to moving the triangle to the right
+    if (x < -1.0f) inc = 0.01f;
+    
+    // get ptr to "offset"
+    GLuint offsetLoc = glGetUniformLocation(renderingProgram, "offset");
+    
+    glProgramUniform1f(renderingProgram, offsetLoc, x);
+    // Use este VAO e suas configurações
+    glBindVertexArray(m_VAO);
+    //Draw Triangle
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+}
+
+int main(void) {
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  //
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            //
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); 	// Resizable option.
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "Lab06.0.4: Use Transformation Function with Shaders", NULL, NULL);
+    glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) {
+		exit(EXIT_FAILURE);
+	}
+    glfwSwapInterval(1);
+    
+    init();
+    
+    while (!glfwWindowShouldClose(window)) {
+        display(glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
---
+++ Laboratory/Lab06.0.4.1-TransformationUsingShadersFunctions/src/vertShader.glsl
@@ -0,0 +1,72 @@
+#version 410
+layout (location=0) in vec3 position;  // coord
+uniform float offset;
+
+// funtions to Transformations(Rotate, Translate, Scale)
+mat4 buildRotateX(float rad);
+mat4 buildRotateY(float rad);
+mat4 buildRotateZ(float rad);
+mat4 buildTranslate(float x, float y, float z);
+mat4 buildScale(float sx, float sy, float sz);
+const float pi = 3.141592653589793;
+
+void main(void) {
+	//All Transformation funtions. 
+	mat4 localTrans = buildTranslate(offset, 0, 0); //Translate only in x axis.
+	mat4 localScal = buildScale(0.25, 0.25, 0); //Scale in 25%.
+	mat4 localRotaX = buildRotateX(offset*pi/4);
+	mat4 localRotaY = buildRotateY(offset*pi/4);
+	mat4 localRotaZ = buildRotateZ(offset*pi/4);
+	
+	gl_Position = localTrans * vec4(position, 1.0);  // right-to-left
+	//gl_Position = localScal * vec4(position, 1.0); 
+	//gl_Position = localRotaZ * vec4(position, 1.0);	    		
+	//gl_Position = localTrans * localScal * vec4(position, 1.0);
+	//gl_Position = localScal * localTrans * vec4(position, 1.0); // Why is more slow?
+	//gl_Position = localTrans * localRotaZ * localScal * vec4(position, 1.0);
+}
+
+// builds and returns a matrix that performs a rotation around the X axis
+mat4 buildRotateX(float rad) {
+    mat4 xrot = mat4(1.0, 0.0,      0.0,       0.0,
+                     0.0, cos(rad), -sin(rad), 0.0,
+                     0.0, sin(rad), cos(rad),  0.0,
+                     0.0, 0.0,      0.0,       1.0);
+    return xrot;
+}
+
+// builds and returns a matrix that performs a rotation around the Y axis
+mat4 buildRotateY(float rad) {
+    mat4 yrot = mat4(cos(rad),  0.0, sin(rad), 0.0,
+                     0.0,       1.0, 0.0,      0.0,
+                     -sin(rad), 0.0, cos(rad), 0.0,
+                     0.0,       0.0, 0.0,      1.0);
+    return yrot;
+}
+
+// builds and returns a matrix that performs a rotation around the Z axis
+mat4 buildRotateZ(float rad) {
+    mat4 zrot = mat4(cos(rad), -sin(rad), 0.0, 0.0,
+                     sin(rad), cos(rad),  0.0, 0.0,
+                     0.0,      0.0,       1.0, 0.0,
+                     0.0,      0.0,       0.0, 1.0);
+    return zrot;
+}
+
+// builds and returns a translation matrix
+mat4 buildTranslate(float x, float y, float z) {
+    mat4 trans = mat4(1.0, 0.0, 0.0, 0.0,
+                      0.0, 1.0, 0.0, 0.0,
+                      0.0, 0.0, 1.0, 0.0,
+                      x,   y,   z,   1.0);
+    return trans;
+}
+
+// builds and returns a Scale matrix
+mat4 buildScale(float sx, float sy, float sz) {
+    mat4 scale = mat4( sx, 0.0, 0.0, 0.0,
+                      0.0,  sy, 0.0, 0.0,
+                      0.0, 0.0,  sz, 0.0,
+                      0.0, 0.0, 0.0, 1.0);
+    return scale;
+}
