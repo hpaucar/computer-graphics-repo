6eed0c14b25f530192ead5557f0944c8b8da179f
Herminio PC-Santa Clara <herminiopaucar@gmail.com>
2020-12-05 01:28:49 -0500

    new app Shaders Vertex

---
+++ Laboratory/USING-SHADERS/LabShaders05-CircleWithTransformationsColors/src/main.cpp
@@ -0,0 +1,86 @@
+//============================================================================
+// Name        : Draw Circle with color and Transformations
+// Professor   : Herminio Paucar
+// Version     :
+// Description :
+//============================================================================
+
+// Include standard headers
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+// Include GLEW
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// Include GLFW
+#include <GLFW/glfw3.h>
+
+#include <string>
+#include <fstream>
+
+#include "Utils.h"
+
+GLuint renderingProgram;
+GLuint m_VBO;
+GLuint m_VAO;
+
+using namespace std;
+
+void init (GLFWwindow* window) {
+	renderingProgram = Utils::createShaderProgram("src/vertShader.glsl", "src/fragShader.glsl");
+
+	// Cria um ID na GPU para um array de  buffers
+	glGenVertexArrays(1, &m_VAO);
+	glBindVertexArray(m_VAO);
+}
+
+void display(GLFWwindow* window, double currentTime) {
+    glUseProgram(renderingProgram);
+    
+    // clear the background to black, each time
+    glClear(GL_DEPTH_BUFFER_BIT);
+    glClearColor(0.0, 0.0, 0.0, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // get ptr to "offset"
+    GLuint rotateLoc = glGetUniformLocation(renderingProgram, "rotate");
+    
+    glProgramUniform1f(renderingProgram, rotateLoc, currentTime);
+    //Draw Triangle
+    glPointSize(20);
+    glDrawArrays(GL_POINTS, 0, 50);
+    //Draw Rectangle with Two Triangles, active 6 vertex positions in VertexShaders
+    //glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+int main(void) {
+	if (!glfwInit()) {
+		exit(EXIT_FAILURE);
+	}
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  //
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            //
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE); 	// Resizable option.
+
+    GLFWwindow* window = glfwCreateWindow(800, 800, "LabShaders05: Cicle With Color and Transforms", NULL, NULL);
+    glfwMakeContextCurrent(window);
+	if (glewInit() != GLEW_OK) {
+		exit(EXIT_FAILURE);
+	}
+    glfwSwapInterval(1);
+    
+    init(window);
+    
+    while (!glfwWindowShouldClose(window)) {
+        display(window, glfwGetTime());
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
---
+++ Laboratory/USING-SHADERS/LabShaders05-CircleWithTransformationsColors/src/vertShader.glsl
@@ -0,0 +1,47 @@
+#version 410
+
+uniform float rotate;
+const float radius = 1;
+const float pi = 3.141592653589793;
+const float twicePi = 2*pi;
+const float numberOfSides = 50;
+out vec3 oColor;
+
+mat4 buildRotateZ(float rad);
+mat4 buildScale(float sx, float sy, float sz);
+
+void main(void) {
+	//gl_VertexID: indica el id de los vertices en el buffer.     
+    if(gl_VertexID%2 == 0)
+    	oColor = vec3(1.0, 0.0, 0.0);
+    else
+    	oColor = vec3(0.0, 1.0, 0.0);
+    
+    float i = gl_VertexID;    
+	float x_vertex = radius * cos(i * twicePi / numberOfSides);
+	float y_vertex = radius * sin(i * twicePi / numberOfSides);
+	
+	mat4 localRotaZ = buildRotateZ(rotate * twicePi / 360);
+	mat4 localScal = buildScale(0.8, 0.8, 0); //Scale in 25%.
+
+	gl_Position = localRotaZ * vec4(x_vertex, y_vertex, 0.0, 1.0);
+	//gl_Position = localScal * localRotaZ * vec4(x_vertex, y_vertex, 0.0, 1.0); 		    		
+}
+
+// builds and returns a matrix that performs a rotation around the Z axis
+mat4 buildRotateZ(float rad) {
+    mat4 zrot = mat4(cos(rad), -sin(rad), 0.0, 0.0,
+                     sin(rad), cos(rad),  0.0, 0.0,
+                     0.0,      0.0,       1.0, 0.0,
+                     0.0,      0.0,       0.0, 1.0);
+    return zrot;
+}
+
+// builds and returns a Scale matrix
+mat4 buildScale(float sx, float sy, float sz) {
+    mat4 scale = mat4( sx, 0.0, 0.0, 0.0,
+                      0.0,  sy, 0.0, 0.0,
+                      0.0, 0.0,  sz, 0.0,
+                      0.0, 0.0, 0.0, 1.0);
+    return scale;
+}
